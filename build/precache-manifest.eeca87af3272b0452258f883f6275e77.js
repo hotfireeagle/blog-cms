21a53132d0ff1a545e74035fb16a09712aa2398a
hotfireeagle <hotfireeagle@163.com>
2020-05-01 22:45:50 +0800

    build: what the fuck forget commit

---
+++ build/precache-manifest.eeca87af3272b0452258f883f6275e77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b806395a1a4b22386cd148f86b27c03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e38386db333511debb29",
+    "url": "/static/css/2.3c515868.chunk.css"
+  },
+  {
+    "revision": "0eb278b0bc56f38891d1",
+    "url": "/static/css/main.5d6e551c.chunk.css"
+  },
+  {
+    "revision": "e38386db333511debb29",
+    "url": "/static/js/2.232f5a7e.chunk.js"
+  },
+  {
+    "revision": "ae69a686e1f284d78b6ec2bbf4577426",
+    "url": "/static/js/2.232f5a7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eb278b0bc56f38891d1",
+    "url": "/static/js/main.8f6d3486.chunk.js"
+  },
+  {
+    "revision": "121bfdc0ddf1f87102a3",
+    "url": "/static/js/runtime-main.b12b798d.js"
+  }
+]);
